31c38a609dc1d7e512d996e1d09ea1aa23282a05
ptrrdrck <56177908+ptrrdrck@users.noreply.github.com>
2021-11-07 17:59:22 -0500

    Remove extra themes

--- index.js
+++ index.js
@@ -8,16 +8,6 @@ const themes = {
     "--primaryColor": "rgba(0, 0, 0, 1)",
     "--secondaryColor": "rgba(255, 255, 255, 1)",
     "--filterColor": "invert(1)"
-  },
-  two: {
-    "--primaryColor": "hsla(203, 100%, 74%, 1)",
-    "--secondaryColor": "hsla(81, 80%, 16%, 1)",
-    "--filterColor": "hue-rotate(100deg)"
-  },
-  three: {
-    "--primaryColor": "hsla(39, 100%, 71%, 1)",
-    "--secondaryColor": "hsla(261, 80%, 16%, 1)",
-    "--filterColor": "hue-rotate(280deg)"
   }
 };
 
